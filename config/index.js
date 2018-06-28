2c609639a7264a0fce812244c3db571f20c1a315
crisgon <kristiano.gon@gmail.com>
2018-06-28 00:56:14 -0300

    Deploy

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'dist/static',
     assetsPublicPath: 'https://crisgon.github.io/vue-cart/',
 
     /**
